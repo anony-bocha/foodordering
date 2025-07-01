24fe9c7ec9ab75e707e2ef7d20a2e7a2abc8d8a0
yonatan semate <semateyonatan@gmail.com>
2025-07-01 12:07:58 +0300

    cart.html and form.py updated

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class CheckoutForm(forms.Form):
-    customer_name = forms.CharField(max_length=100)
-    customer_email = forms.EmailField()
-    customer_address = forms.CharField(widget=forms.Textarea)
+    customer_name = forms.CharField(max_length=100, label="Name")
+    customer_email = forms.EmailField(label="Email")
+    customer_address = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), label="Address")
